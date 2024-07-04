d14283a4598758b2751a28967f74815630b94841
Mozzarella <32901205+mosnatchar@users.noreply.github.com>
2024-07-04 20:21:52 +0700

    Create palindrome.swift

---
+++ palindrome.swift
@@ -0,0 +1,11 @@
+import Foundation
+// let exampleString = "askka"
+  //       if isPalindrome(exampleString) {
+  //           print("\"\(exampleString)\" is a palindrome")
+  //       } else {
+  //           print("\"\(exampleString)\" is not a palindrome")
+  //       }
+ func isPalindrome(_ str: String) -> Bool {
+        let cleanedStr = str.lowercased().filter { $0.isLetter }
+        return cleanedStr == String(cleanedStr.reversed())
+}
